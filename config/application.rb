e09ed50e45ad83d9cd163f18b4eb017aa98e8702
Zach Schwartz <zachatorcodes@gmail.com>
2024-04-10 14:44:35 -0600

    Enable cookies and sessions middleware

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,12 @@ module Freshnest
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Adds cookies and session middleware for use in application
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    # Use SameSite=Strict for all cookies to help protect against CSRF (Cross-Site Request Forgery)
+    config.action_dispatch.cookies_same_site_protection = :strict
   end
 end
